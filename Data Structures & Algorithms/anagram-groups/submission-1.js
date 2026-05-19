8407e8aff91b09e99c036a4b782b090d7d0bcc71
Mitch <wrightmitchell@hotmail.com>
2026-05-18 23:28:30 -0700

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const results = []
+        const hash = {}
+
+        // loop over strs
+        // for each loop, create a 26 letter key
+        // check if key is in hash. if so, push to hash arr value else create new entry
+
+        for (let str of strs) {
+            let count = new Array(26).fill(0)
+            for (let i = 0; i < str.length; i++) {
+                count[str.charCodeAt(i) - 'a'.charCodeAt(0)]++
+            }
+            const key = count.join(',')
+            if (hash[key]) {
+                hash[key].push(str)
+            } else {
+                hash[key] = [str]
+            }
+        }
+
+        for (let group in hash) {
+            results.push(hash[group])
+        }
+
+        return results
+    }
+}
+
+
