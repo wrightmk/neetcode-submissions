9dc99738f2c3be7e6c16b72398e62d55e13608eb
Mitch <wrightmitchell@hotmail.com>
2026-05-19 23:17:59 -0700

    Add: longest-consecutive-sequence - submission-4

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-4.js
@@ -0,0 +1,41 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        const hash = {}
+        for (let num of nums) {
+            hash[num] = true
+        }
+
+        for (let h in hash) {
+            if (parseInt(h) - 1 in hash) {
+                hash[h] = false
+            }
+        }
+
+        let largest = 0
+        for (let h in hash) {
+            if (hash[h]) {
+                // 2, 10, 20
+                let starter = [parseInt(h), 1]
+                let counter = 1
+                while (counter <= nums.length) {
+                    if (String(starter[0] + 1) in hash) {
+                        starter[0]++
+                        starter[1]++
+                    }
+                    if (starter[1] > largest) {
+                        largest = starter[1]
+                    }
+                    counter++
+                }
+            }
+        }
+
+        
+        console.log(hash)
+        return largest
+    }
+}
